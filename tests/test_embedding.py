00274ab5aa3b0a10b478009f0999d59b96cb8c48
Logan Hallee <lhallee@udel.edu>
2025-03-14 13:41:49 -0400

    update

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -17,7 +17,7 @@ def generate_random_sequence(length: int) -> str:
 
 
 if __name__ == "__main__":
-    # py tests/test_embedding_esmfast.py
+    # py tests/test_embedding.py
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def test_embedding(model, sequences):
